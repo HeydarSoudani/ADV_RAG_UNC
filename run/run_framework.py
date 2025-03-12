92d7012f0e35367ed82b02ee835ca3105c3563b5
Heydar Soudani <heydar.soudani@gmail.com>
2025-03-12 19:13:40 +0100

    first commit

---
+++ run/run_framework.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import torch
+import argparse
+
+from utils.utils import set_seed
+from adaptive_generation import adaptive_generation
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='meta-llama/Llama-2-7b-chat-hf')
+    parser.add_argument('--dataset', type=str, default='nqgold', choices=[
+        '2wikimultihopqa', 'hotpotqa', 'musique', 'iirc', 'multihop_rag',
+        'nqgold', 'trivia', 'popqa',
+        'factscore'
+    ])
+    parser.add_argument('--subsec', type=str, default='dev', choices=['train', 'dev', 'test', 'validation'])
+    parser.add_argument('--rag_type', type=str, default='wo_rag', choices=[
+        'wo_rag', 'sr_rag', 'fl_rag', 'fs_rag',
+        'flare', 'dragin'
+    ])
+    parser.add_argument('--retriever', type=str, default='bm25', choices=[
+        'positive', 'negative', 'bm25', 'contriever', 'rerank', 'bge_m3', 'sgpt' # https://github.com/Muennighoff/sgpt
+    ])
+    parser.add_argument('--accuracy_metric', type=str, default="exact_match", choices=[
+        'exact_match', 'model_judge', 'bem_score', 'bert_score', 'rouge_score'
+    ])
+    parser.add_argument('--model_eval', type=str, default='gpt-3.5-turbo') # meta-llama/Llama-3.1-8B-Instruct
+    parser.add_argument('--fraction_of_data_to_use', type=float, default=1.0)
+    parser.add_argument("--roc_auc_threshold", type=float, default=0.8)
+    parser.add_argument('--num_generations', type=int, default=10)
+    parser.add_argument('--max_new_tokens', type=int, default=32)
+    parser.add_argument('--decoding_method', type=str, default='beam_search')
+    parser.add_argument('--temperature', type=float, default='1.0')
+    parser.add_argument('--num_beams', type=int, default='1')
+    parser.add_argument('--top_p', type=float, default=1.0)
+    parser.add_argument('--device', type=int, default=0)
+    parser.add_argument('--run', type=str, default='run_0')
+    parser.add_argument("--seed", type=int, default=10)
+    args = parser.parse_args()
+    
+    ### === Define CUDA device =================== 
+    args.output_dir = f"run_output/{args.run}" 
+    args.device = torch.device("cuda:" + str(args.device) if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        print(f"Number of available GPUs: {torch.cuda.device_count()}")
+        for i in range(torch.cuda.device_count()):
+            print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+    else:
+        print("CUDA is not available. No GPUs detected.")
+        
+    
+    ### === Run Steps ============================
+    set_seed(args.seed)
+    adaptive_generation(args)
---
+++ scripts/run_adaptive_framework.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=gpu
+#SBATCH --time=0:45:00
+#SBATCH --output=script_logging/slurm_%A.out
+
+module load 2024
+module load Python/3.12.3-GCCcore-13.3.0
+# module load Java/21.0.2
+# pip install --upgrade transformers
+
+
+
+### === Set variables ==========================
+model="meta-llama/Llama-3.1-8B-Instruct"
+dataset="2wikimultihopqa"
+subsec="test"
+rag_type="wo_rag"
+retriever="bm25"
+fraction_of_data_to_use=0.6    # nqgold 0.104 | trivia 0.034 | popqa 0.021 | 2wikimultihopqa 0.6
+accuracy_metric="exact_match"    # model_judge | exact_match
+model_eval="gpt-3.5-turbo"
+run="run_1 (300s-EM)"          # run_0 (300s-G3.5) | run_1 (300s-EM)
+
+srun python $HOME/RAG_UNC/_truth_torch_framework/run/run_framework.py \
+    --model "$model" \
+    --dataset "$dataset" \
+    --subsec "$subsec" \
+    --prompt_format "$prompt_format" \
+    --fraction_of_data_to_use "$fraction_of_data_to_use"\
+    --accuracy_metric "$accuracy_metric" \
+    --model_eval "$model_eval" \
+    --run "$run" \
+
+
+
+# prompt_format:
+    # 'only_q', 'q_negative', 'q_positive',
+    # 'bm25_retriever_top1', 'bm25_retriever_top5',
+    # 'contriever_retriever_top1', 'contriever_retriever_top5',
+    # 'rerank_retriever_top1', 'rerank_retriever_top5'
+
+# rag_type:
+    # 'wo_rag', 'sr_rag', 'fl_rag', 'fs_rag',       
+    # 'flare', 'dragin' 
+
+# retriever:
+    # 'positive', 'negative', 'bm25', 'contriever', 'rerank', 'bge_m3', 'sgpt'
+
+# Datasets:
+    # '2wikimultihopqa', 'hotpotqa', 'musique', 'iirc',
+    # 'nqgold', 'trivia', 'popqa',
+    # 
+
+# Model name:
+    # GPT-4o:     "openai/gpt-4o"
+    # GPT-3.5:    "openai/gpt-3.5-turbo-instruct"
+    
+    # llama3.2:   "meta-llama/Llama-3.2-3B-Instruct"
+    # llama3.2:   "meta-llama/Llama-3.2-1B-Instruct"
+    # llama3.1:   "meta-llama/Llama-3.1-8B-Instruct"
+    # llama2:     "meta-llama/Llama-2-7b-chat-hf"
+    # Qwen2.5:    "Qwen/Qwen2.5-7B-Instruct"
+    # Gemma2:     "google/gemma-2-9b-it"
+    # Phi4 (14B): "microsoft/phi-4"
+    # Mistral:    "mistralai/Mistral-7B-Instruct-v0.3"
+
+    # DS-7B: deepseek-ai/DeepSeek-R1-Distill-Qwen-7B
+    # DS-8B: deepseek-ai/DeepSeek-R1-Distill-Llama-8B
+    # DS-14B: deepseek-ai/DeepSeek-R1-Distill-Qwen-14B
+    
+    
+    
+
